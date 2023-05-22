03a995d2cb31216dab23ba4f88c7d1d0423d1db3
eliasef <eliasef0305@gmail.com>
2023-05-22 15:07:32 -0300

    :sparkles: feat: finalizando About.tsx

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -6,7 +6,9 @@ import { useTheme } from "styled-components";
 import { Feather } from '@expo/vector-icons';
 
 import circle from '../../assets/img/circle.png';
+import dots from '../../assets/img/dots.png';
 import * as S from './styles';
+import { FadeAnimation } from "../../components/FadeAnimation";
 
 type RouteParams = {
     pokemonId: number,
@@ -54,6 +56,7 @@ type PokemonProps = {
 export function About() {
 
     const route = useRoute();
+    const { goBack } = useNavigation()
     const { pokemonId } = route.params as RouteParams;
     const { backgroundCard }: any = useTheme();
 
@@ -85,23 +88,62 @@ export function About() {
 
     }, []);
 
-    
+    function handleGoBack() {
+        goBack()
+    }
+
     return <>
         {load ? <>
             <Text style={{ marginTop: 200 }}>Carregando...</Text>
-        </> : 
-            <ScrollView style={{ flex: 1 }}>
+        </> :
+            <ScrollView style={{ flex: 1, backgroundColor: '#fff' }}>
                 <S.Header type={pokemon.types[0].type.name}>
-                    <S.BackButton>
+                    <S.BackButton onPress={handleGoBack}>
                         <Feather name="chevron-left" size={24} color="#fff" />
                     </S.BackButton>
-                    {/* <S.ContentImage>
+                    <S.ContentImage>
                         <S.CircleImage source={circle} />
-                        <S.PokemonImage source={{ 
-                            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
-                         }} />
-                    </S.ContentImage> */}
+                        <FadeAnimation>
+                            <S.PokemonImage source={{
+                                uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+                            }} />
+                        </FadeAnimation>
+                    </S.ContentImage>
+
+                    <S.Content>
+                        <S.PokemonId>{pokemon.id}</S.PokemonId>
+                        <S.PokemonName>{pokemon.name}</S.PokemonName>
+
+                        <S.PokemonTypeContainer>
+                            {pokemon.types.map(({ type }) =>
+                                <S.PokemonType type={type.name} key={type.name}>
+                                    <S.PokemonTypeText>{type.name}</S.PokemonTypeText>
+                                </S.PokemonType>)}
+                        </S.PokemonTypeContainer>
+                    </S.Content>
+                    <S.DotsImage source={dots} />
                 </S.Header>
+
+                <S.Container>
+                    <S.Title type={pokemon.types[0].type.name}>Base States</S.Title>
+
+                    {pokemon.stats.map(attribute =>
+                        <S.StatusBar key={attribute.stat.name}>
+                            <S.Attributes>{attribute.stat.name}</S.Attributes>
+                            <S.AttributeValue>{attribute.base_stat}</S.AttributeValue>
+
+                            <S.ContentBar>
+                                <S.ProgressBar
+                                    type={pokemon.types[0].type.name}
+                                    borderWidth={0}
+                                    progress={100}
+                                    width={attribute.base_stat}
+                                    color={pokemon.color}
+                                />
+                            </S.ContentBar>
+                        </S.StatusBar>
+                    )}
+                </S.Container>
             </ScrollView>
         }
     </>
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from "styled-components/native";
 import { TypeName } from ".";
 
+import * as Progress from 'react-native-progress'
+
 type TypeProps = {
-    type: TypeName
+  type: TypeName
 };
 
 export const Header = styled.View<TypeProps>`
@@ -22,3 +24,134 @@ export const BackButton = styled.TouchableOpacity`
     left: 40px;
 `;
 
+export const ContentImage = styled.View`
+    position: relative;
+`;
+
+export const CircleImage = styled.Image`
+  width: 125px;
+  height: 125px;
+  position: absolute;
+`;
+
+export const PokemonImage = styled.Image`
+  width: 125px;
+  height: 125px;
+`
+
+export const Content = styled.View`
+  margin-left: 30px;
+`;
+
+export const PokemonId = styled.Text`
+    ${({ theme }: { theme: any }) => css`
+      font-size: 16px;
+      line-height: 19px;
+      font-style: normal;
+      font-weight: bold;
+      color: ${theme.colors.light_text};
+  `}
+`;
+
+export const PokemonName = styled.Text`
+    ${({ theme }: { theme: any }) => css`
+      text-transform: capitalize;
+      font-size: normal;
+      font-weight: bold;
+      font-size: 28px;
+      line-height: 38px;
+      color: ${theme.colors.background};
+  `}
+`;
+
+export const PokemonTypeContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const PokemonType = styled.View<TypeProps>`
+    ${({ theme, type }: { theme: any, type: any }) => css`
+      width: 61px;
+      height: 25px;
+      background: ${theme.boxType[type]};
+      border-radius: 3px;
+      justify-content: center;
+      align-items: center;
+      margin-left: 5px;
+      margin-top: 10px;
+  `}  
+`;
+
+export const PokemonTypeText = styled.Text`
+    ${({ theme }: { theme: any }) => css`
+      color: ${theme.colors.background};
+      font-weight: 500;
+      font-size: 12px;
+      line-height: 14px;
+      font-style: normal;
+  `}  
+`;
+
+export const DotsImage = styled.View`
+  width: 85px;
+  position: absolute;
+  right: -20px;
+  top: 220px;
+`;
+
+export const Container = styled.View`
+    ${({ theme }: { theme: any }) => css`
+      flex: 1;
+      padding: 20px;
+      background-color: ${theme.colors.background};
+      border-top-right-radius: 40px;
+      border-top-left-radius: 40px;
+      margin-top: -40px;
+  `} 
+`;
+
+export const Title = styled.Text<TypeProps>`
+    ${({ theme, type }: { theme: any, type: any }) => css`
+      font-style: normal;
+      font-weight: bold;
+      font-size: 16px;
+      line-height: 19px;
+      padding: 20px;
+      color: ${theme.boxType[type]};
+  `}  
+`;
+
+export const StatusBar = styled.View`
+    width: 100%;
+    padding: 10px 20px;
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Attributes = styled.Text`
+    ${({ theme }: { theme: any }) => css`
+      font-style: normal;
+      font-weight: 500;
+      font-size: 12px;
+      line-height: 14px;
+      text-transform: capitalize;
+      color: ${theme.colors.light_text};
+  `} 
+`;
+
+export const AttributeValue = styled.Text`
+    ${({ theme }: { theme: any }) => css`
+      font-style: normal;
+      font-weight: normal;
+      font-size: 16px;
+      line-height: 19px;
+      text-align: right;
+      color: ${theme.colors.detail};
+      margin-left: 20px;
+  `} 
+`;
+
+export const ContentBar = styled.View`
+  margin-left: 20px;
+`;
+
+export const ProgressBar = styled(Progress.Bar)<TypeProps>``;
